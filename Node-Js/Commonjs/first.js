6d33e8f94cbe55515ec38caf2787872695818f02
maambaku.lokesh <mambakulokesh4756@gmail.com>
2024-11-12 21:26:49 +0530

    "common-js"

---
+++ Node-Js/Commonjs/first.js
@@ -0,0 +1,9 @@
+function f1() {
+  console.log("f1 Called");
+}
+
+function f2() {
+  console.log("f2 called");
+}
+
+module.exports = { f1, f2 };
---
+++ Node-Js/Commonjs/second.js
@@ -0,0 +1,6 @@
+var funs = require("./first.js");
+
+funs.f1();
+funs.f2();
+
+
